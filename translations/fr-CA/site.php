1702668d5e8df8d7cb3da4a1bfd55e5c3d55c6e0
Julien Lambert-Charette <44408591+jlcharette@users.noreply.github.com>
2019-06-27 10:07:31 -0400

    Feature/wet (#1) (#2)
    * Homepage Integration. Matrix foundation.
    
    * Remove AWS keypair from git tracking.

---
+++ translations/fr-CA/site.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'Canada School of Public Service' => 'École de la fonction publique du Canada',
+    'Government of Canada' => 'Gouvernement du Canada',
+
+    // Global Header
+    'Language selection' => 'Sélection de la langue',
+    'Search Canada.ca' => 'Rechercher dans Canada.ca',
+    'Skip to main content' => 'Passer au contenu principal',
+    'Skip to About Government' => 'Passer à « Au sujet du gouvernement »',
+
+    // Breadcrumb
+    'Home' => 'Accueil',
+
+    // Footer
+    'Report a problem or mistake on this page' => 'Signaler un problème ou une erreur sur cette page',
+    'Share this page' => 'Partagez cette page',
+    'Date modified:' => 'Date de modification :',
+    'About government' => 'Au sujet du gouvernement',
+    'About this site' => 'Au sujet de ce site',
+    'Stay connected' => 'Rester connecté',
+    'Contact us' => 'Contactez-nous',
+
+    // Footer Landscape Links
+    'https://www.canada.ca/en/contact.html' => 'https://www.canada.ca/fr/contact.html',
+    'Departments and agencies' => 'Ministères et organismes',
+    'https://www.canada.ca/en/government/dept.html' => 'https://www.canada.ca/fr/gouvernement/min.html',
+    'Public service and military' => 'Fonction publique et force militaire',
+    'https://www.canada.ca/en/government/publicservice.html' => 'https://www.canada.ca/fr/gouvernement/fonctionpublique.html',
+    'News' => 'Nouvelles',
+    'https://www.canada.ca/en/news.html' => 'https://www.canada.ca/fr/nouvelles.html',
+    'Treaties, laws and regulations' => 'Traités, lois et règlements',
+    'https://www.canada.ca/en/government/system/laws.html' => 'https://www.canada.ca/fr/gouvernement/systeme/lois.html',
+    'Government-wide reporting' => 'Rapports à l\'échelle du gouvernement',
+    'https://www.canada.ca/en/transparency/reporting.html' => 'https://www.canada.ca/fr/transparence/rapports.html',
+    'Prime Minister' => 'Premier ministre',
+    'http://pm.gc.ca/eng' => 'http://pm.gc.ca/fra',
+    'How government works' => 'Comment le gouvernement fonctionne',
+    'https://www.canada.ca/en/government/system.html' => 'https://www.canada.ca/fr/gouvernement/systeme.html',
+    'Open government' => 'Gouvernement ouvert',
+    'http://open.canada.ca/en/' => 'http://ouvert.canada.ca/',
+    
+    // Footer Brand Links
+    'Social media' => 'Médias sociaux',
+    'https://www.canada.ca/en/social.html' => 'https://www.canada.ca/fr/sociaux.html',
+    'Mobile applications' => 'Applications mobiles',
+    'https://www.canada.ca/en/mobile.html' => 'https://www.canada.ca/fr/mobile.html',
+    'About Canada.ca' => 'À propos de Canada.ca',
+    'https://www.canada.ca/en/government/about.html' => 'https://www.canada.ca/fr/gouvernement/a-propos.html',
+    'Terms and conditions' => 'Avis',
+    'https://www.canada.ca/en/transparency/terms.html' => 'https://www.canada.ca/fr/transparence/avis.html',
+    'Privacy' => 'Confidentialité',
+    'https://www.canada.ca/en/transparency/privacy.html' => 'https://www.canada.ca/fr/transparence/confidentialite.html',
+
+];
