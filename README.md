82844f876731ce1d397fff01e331ff13c850664a
Julien Lambert-Charette <julien.lambert-charette@canada.ca>
2019-11-28 07:50:50 -0500

    Refactor on Craft-WET

---
+++ README.md
@@ -0,0 +1,41 @@
+# Craft CMS WET-BOEW Boilerplate
+
+GCWeb v5.1
+WET-BOEW v4.0.31
+
+IMPORTANT: THIS IS ONLY A BOILERPLATE.
+
+# Requirements
+
+- Craft CMS server requirements
+- Composer
+- Npm
+
+# Install
+
+Before you install this boilerplate, check if you meet all Craft CMS server requirements.
+
+1. Create a new database for Craft CMS
+2. Clone the repository
+3. Run `composer install`, `npm install` and `npm run dev` from the project root
+4. Rename `.env.example` to `.env` and modify the environment variables to use your own database credentials
+5. Run `./craft setup` from the project root, follow the instructions
+6. Log to the `/admin` area, click on `Entries` in the sidebar and select `Craft WET EN` in the dropdown menu
+7. Click `New entry`, give it a title and set the slug to `__home__`, then save the entry
+
+# Q & A
+
+Q: The NPM packages won't install properly, how can I fix this?
+A: Remove the `node_modules` folder and try the `npm install` command again. If that still doesn't work, you will need to run `npm install --save-dev PackageName` one by one. You can find an array of all npm packages used in the `package.json` file under `devDependencies`.
+
+Q: What if I don't want the home page to be an entry within the `Pages` structure?
+A: One solution would be to create a new `single` section and a new template.
+
+# Roadmap
+
+- Finish the Homepage template.
+- Create documentation, FAQs, etc.
+- Remove the "Form Builder" to a standalone component.
+- Remove the "Menu Builder" to a standalone component.
+
+Craft-WET will need a heavy refactor once the next version of Craft (4) and WET (5) are released.
---
+++ translations/fr/site.php
@@ -0,0 +1,154 @@
+<?php
+
+return [
+
+    // Branding
+    "Canada School of Public Service" => "École de la fonction publique du Canada",
+    "Government of Canada" => "Gouvernement du Canada",
+
+    // Header
+    "Language selection" => "Sélection de la langue",
+    "Search Canada.ca" => "Rechercher dans Canada.ca",
+    "Search this website" => "Rechercher dans ce site web",
+    "Skip to main content" => "Passer au contenu principal",
+    "Skip to About Government" => "Passer à « Au sujet du gouvernement »",
+    "https://www.canada.ca/en/school-public-service/search.html" => "https://www.canada.ca/fr/ecole-fonction-publique/rechercher.html",
+    "Sign-on information" => "Information de l'utilisateur actuel",
+    "Signed in as" => "Connecté en tant que",
+    "Account settings" => "Paramètres du compte",
+    "Sign out" => "Fermer la session",
+    "We have archived this page and will not be updating it." => "Nous avons archivé cette page et elle ne sera plus mise à jour.",
+    "You can use it for research or reference." => "Vous pouvez la consulter à des fins de recherche ou à titre de référence.",
+    "Close overlay" => "Fermer la fenêtre superposée",
+    "GCcampus login" => "Se connecter à GCcampus",
+
+    // Ajax submenu
+    "/dist/site/ajax/sitemenu-v5-en.html" => "/dist/site/ajax/sitemenu-v5-fr.html",
+
+    // Menu
+    "https://www.canada.ca/en/services/jobs.html" => "https://www.canada.ca/fr/services/emplois.html",
+    "Jobs and the workplace" => "Emplois et milieu de travail",
+    "https://www.canada.ca/en/services/immigration-citizenship.html" => "https://www.canada.ca/fr/services/immigration-citoyennete.html",
+    "Immigration and citizenship" => "Immigration et citoyenneté",
+    "https://travel.gc.ca/" => "https://voyage.gc.ca/",
+    "Travel and tourism" => "Voyage et tourisme",
+    "https://www.canada.ca/en/services/business.html" => "https://www.canada.ca/fr/services/entreprises.html",
+    "Business and industry" => "Entreprises et industrie",
+    "https://www.canada.ca/en/services/benefits.html" => "https://www.canada.ca/fr/services/prestations.html",
+    "Benefits" => "Prestations",
+    "https://www.canada.ca/en/services/health.html" => "https://www.canada.ca/fr/services/sante.html",
+    "Health" => "Santé",
+    "https://www.canada.ca/en/services/taxes.html" => "https://www.canada.ca/fr/services/impots.html",
+    "Taxes" => "Impôts",
+    "https://www.canada.ca/en/services/environment.html" => "https://www.canada.ca/fr/services/environnement.html",
+    "Environment and natural resources" => "Environnement et ressources naturelles",
+    "https://www.canada.ca/en/services/defence.html" => "https://www.canada.ca/fr/services/defense.html",
+    "National security and defence" => "Sécurité nationale et défense",
+    "https://www.canada.ca/en/services/culture.html" => "https://www.canada.ca/fr/services/culture.html",
+    "Culture, history and sport" => "Culture, histoire et sport",
+    "https://www.canada.ca/en/services/policing.html" => "https://www.canada.ca/fr/services/police.html",
+    "Policing, justice and emergencies" => "Services de police, justice et urgences",
+    "https://www.canada.ca/en/services/transport.html" => "https://www.canada.ca/fr/services/transport.html",
+    "Transport and infrastructure" => "Transport et infrastructure",
+    "http://international.gc.ca/world-monde/index.aspx?lang=eng" => "http://international.gc.ca/world-monde/index.aspx?lang=fra",
+    "Canada and the world" => "Canada et le monde",
+    "https://www.canada.ca/en/services/finance.html" => "https://www.canada.ca/fr/services/finance.html",
+    "Money and finances" => "Argent et finances",
+    "https://www.canada.ca/en/services/science.html" => "https://www.canada.ca/fr/services/science.html",
+    "Science and innovation" => "Science et innovation",
+
+    // Breadcrumb
+    "Home" => "Accueil",
+    "You are here:" => "Vous êtes ici :",
+
+    // Footer
+    "Report a problem or mistake on this page" => "Signaler un problème ou une erreur sur cette page",
+    "Share this page" => "Partagez cette page",
+    "Date modified:" => "Date de modification :",
+    "About government" => "Au sujet du gouvernement",
+    "About this site" => "Au sujet de ce site",
+    "Stay connected" => "Rester connecté",
+    "Contact us" => "Contactez-nous",
+
+    "https://www.canada.ca/en/contact.html" => "https://www.canada.ca/fr/contact.html",
+    "Departments and agencies" => "Ministères et organismes",
+    "https://www.canada.ca/en/government/dept.html" => "https://www.canada.ca/fr/gouvernement/min.html",
+    "Public service and military" => "Fonction publique et force militaire",
+    "https://www.canada.ca/en/government/publicservice.html" => "https://www.canada.ca/fr/gouvernement/fonctionpublique.html",
+    "News" => "Nouvelles",
+    "https://www.canada.ca/en/news.html" => "https://www.canada.ca/fr/nouvelles.html",
+    "Treaties, laws and regulations" => "Traités, lois et règlements",
+    "https://www.canada.ca/en/government/system/laws.html" => "https://www.canada.ca/fr/gouvernement/systeme/lois.html",
+    "Government-wide reporting" => "Rapports à l'échelle du gouvernement",
+    "https://www.canada.ca/en/transparency/reporting.html" => "https://www.canada.ca/fr/transparence/rapports.html",
+    "Prime Minister" => "Premier ministre",
+    "http://pm.gc.ca/eng" => "http://pm.gc.ca/fra",
+    "How government works" => "Comment le gouvernement fonctionne",
+    "https://www.canada.ca/en/government/system.html" => "https://www.canada.ca/fr/gouvernement/systeme.html",
+    "Open government" => "Gouvernement ouvert",
+    "http://open.canada.ca/en/" => "http://ouvert.canada.ca/",
+
+    "Social media" => "Médias sociaux",
+    "https://www.canada.ca/en/social.html" => "https://www.canada.ca/fr/sociaux.html",
+    "Mobile applications" => "Applications mobiles",
+    "https://www.canada.ca/en/mobile.html" => "https://www.canada.ca/fr/mobile.html",
+    "About Canada.ca" => "À propos de Canada.ca",
+    "https://www.canada.ca/en/government/about.html" => "https://www.canada.ca/fr/gouvernement/a-propos.html",
+    "Terms and conditions" => "Avis",
+    "https://www.canada.ca/en/transparency/terms.html" => "https://www.canada.ca/fr/transparence/avis.html",
+    "Privacy" => "Confidentialité",
+    "https://www.canada.ca/en/transparency/privacy.html" => "https://www.canada.ca/fr/transparence/confidentialite.html",
+
+    // Follow Box Block
+    "Follow:" => "Suivez :",
+
+    // Media Player Blocks
+    "Transcript" => "Transcription",
+    "Cue point" => "Marqueur temporel",
+
+    // Misc
+    "Learn more" => "En savoir plus",
+    "English" => "Français",
+    "All forms" => "Tous les formulaires",
+    "Send" => "Envoyer",
+
+    // Courses Filters
+    "Title" => "Titre",
+    "Type" => "Mode",
+    "Topic" => "Sujet",
+    "Community" => "Communauté",
+    "Audience" => "Public cible",
+    "Business Line" => "Secteur d'activité",
+    "All" => "Tous",
+    "Filter courses" => "Filtrer les cours",
+    "Clear filters" => "Effacer les filtres",
+
+    // Course Entry Metadata
+    "Type:" => "Mode :",
+    "Duration:" => "Durée :",
+    "Topics:" => "Sujets :",
+    "Community:" => "Communauté :",
+    "Audience:" => "Public cible :",
+    "Business line:" => "Secteur d'activité :",
+    "Provider:" => "Fournisseur :",
+    "hours" => "heures",
+    "Upcoming offerings" => "Offres à venir",
+    "To register, you will be prompted to log in to your <a href='https://www.csps-efpc.gc.ca/gccampus-eng.aspx'>GCcampus account</a>." => "Pour vous inscrire, vous serez invité à vous connecter à votre <a href='https://www.csps-efpc.gc.ca/gccampus-fra.aspx'>compte GCcampus</a>.",
+    // Course Offerings
+    "Sorry! There is no offering available at moment." => "Désolé! Il n'y a pas d'offre pour le moment.",
+    "Location" => "Emplacement",
+
+    // Events
+    "Upcoming events" => "Événements à venir",
+    "Title" => "Titre",
+    "Date" => "Date",
+    "Location" => "Lieu",
+    "Language" => "Langue",
+
+    // Calendar
+    "Learning links" => "Liens d'apprentissage",
+    "Courses" => "Cours",
+    "Events" => "Événements",
+    "Videos on demand" => "Vidéos sur demande",
+    "This page is a <b>beta release</b> and only displays upcoming offerings until <b>March 31, 2020</b>. <a href=\"#comments\">Let us know what you think of it</a>." => "Cette page est une <b>version bêta</b> et affiche les offres à venir jusqu'au <b>31 mars 2020</b>. <a href=\"#commentaires\">Dites-nous ce que vous en pensez</a>",
+];
